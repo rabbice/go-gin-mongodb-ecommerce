b9f54789cf878d9d5953d734fbbb82b48dd4fbd9
Ian Maru <iankater@gmail.com>
2022-03-23 13:51:27 +0300

    implemented search product feature

--- src/backend/controllers/productController.go
+++ src/backend/controllers/productController.go
@@ -2,9 +2,9 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
-	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -27,35 +27,31 @@ func AddProduct() gin.HandlerFunc {
 				"error": err.Error()})
 			return
 		}
+
 		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
-		var shop models.Shop
 		var product models.Product
 
 		if err := c.BindJSON(&product); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		}
+		defer cancel()
 
 		validationErr := validate.Struct(product)
 		if validationErr != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
 		}
-		err := shopCollection.FindOne(ctx, bson.M{"shop_id": product.Shop_ID}).Decode(&shop)
-		defer cancel()
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Shop was not found"})
-			return
-		}
 		product.CreatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
 		product.UpdatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
 		product.ID = primitive.NewObjectID()
-		product.Product_ID = product.ID.Hex()
 
 		_, insertErr := productCollection.InsertOne(ctx, product)
 		if insertErr != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Product was not created"})
+			fmt.Println(insertErr)
 			return
 		}
 		defer cancel()
+
 		c.JSON(http.StatusOK, product)
 	}
 }
@@ -65,40 +61,19 @@ func GetProducts() gin.HandlerFunc {
 
 		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 
-		recordPerPage, err := strconv.Atoi(c.Query("recordPerPage"))
-		if err != nil || recordPerPage < 1 {
-			recordPerPage = 10
-		}
+		var products []bson.M
 
-		page, err := strconv.Atoi(c.Query("page"))
-		if err != nil || page < 1 {
-			page = 1
-		}
+		cursor, err := productCollection.Find(ctx, bson.M{})
 
-		startIndex := (page - 1) * recordPerPage
-		startIndex, _ = strconv.Atoi(c.Query("startIndex"))
-
-		matchStage := bson.D{primitive.E{Key: "$match", Value: bson.D{{}}}}
-		groupStage := bson.D{primitive.E{Key: "$group", Value: bson.D{primitive.E{Key: "_id", Value: bson.D{primitive.E{Key: "_id", Value: "null"}}}, {Key: "total_count", Value: bson.D{primitive.E{Key: "$sum", Value: 1}}}, {Key: "data", Value: bson.D{primitive.E{Key: "$push", Value: "$$ROOT"}}}}}}
-		projectStage := bson.D{
-			primitive.E{
-				Key: "$project", Value: bson.D{
-					{Key: "_id", Value: 0},
-					{Key: "total_count", Value: 1},
-					{Key: "product_items", Value: bson.D{primitive.E{Key: "$slice", Value: []interface{}{"$data", startIndex, recordPerPage}}}},
-				}}}
-
-		result, err := productCollection.Aggregate(ctx, mongo.Pipeline{
-			matchStage, groupStage, projectStage})
-		defer cancel()
 		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while listing food items"})
+			c.IndentedJSON(500, "Internal Server Error")
 		}
-		var allProducts []bson.M
-		if err = result.All(ctx, &allProducts); err != nil {
-			log.Fatal(err)
+		defer cancel()
+		if err = cursor.All(ctx, &products); err != nil {
+			c.IndentedJSON(500, "Internal Server Error")
+			return
 		}
-		c.JSON(http.StatusOK, allProducts[0])
+		c.IndentedJSON(200, products)
 	}
 }
 
@@ -162,6 +137,8 @@ func UpdateProduct() gin.HandlerFunc {
 			Key: "_id", Value: objectId}},
 			bson.D{primitive.E{Key: "$set", Value: bson.D{primitive.E{Key: "name", Value: product.Name},
 				primitive.E{Key: "description", Value: product.Description},
+				primitive.E{Key:"price", Value: product.Price},
+				primitive.E{Key:"image", Value: product.Image},
 			}}})
 		if err != nil {
 			c.IndentedJSON(http.StatusInternalServerError, gin.H{
@@ -172,6 +149,7 @@ func UpdateProduct() gin.HandlerFunc {
 			"message": "Product successfully updated"})
 	}
 }
+
 
 func SearchProductByQuery() gin.HandlerFunc {
 	return func(c *gin.Context) {
--- src/backend/models/productModel.go
+++ src/backend/models/productModel.go
@@ -9,13 +9,12 @@ import (
 type Product struct {
 	ID          primitive.ObjectID `bson:"_id"`
 	Name        *string            `json:"name" validate:"required"`
-	Description string             `json:"description"`
-	Price       float64            `json:"price" validate:"required"`
-	Image       string             `json:"image"`
-	Tags        []string           `json:"tags"`
+	Description *string            `bson:"description" json:"description"`
+	Price       *float64           `bson:"price" json:"price" validate:"required"`
+	Image       *string            `bson:"image" json:"image"`
+	Tags        []string           `bson:"tags" json:"tags"`
 	CreatedAt   time.Time          `json:"created_at"`
 	UpdatedAt   time.Time          `json:"updated_at"`
 	DeletedAt   time.Time          `json:"deleted_at"`
-	Product_ID  string             `json:"product_id"`
 	Shop_ID     *string            `json:"shop_id" validate:"required"`
 }
