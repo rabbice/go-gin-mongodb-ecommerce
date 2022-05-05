3de6fc0774872e8d54253f3a634beb27554b2e7c
Ian Maru <iankater@gmail.com>
2022-05-05 15:32:48 +0300

    application metrics feature added

--- routes/sellerRoutes.go
+++ routes/sellerRoutes.go
@@ -14,4 +14,6 @@ func SellerRoutes(v1 *gin.Engine) {
 	v1.POST("/product", controllers.AddProduct())
 	v1.DELETE("/product/:product_id", controllers.DeleteProduct())
 	v1.PUT("/product/:product_id", controllers.UpdateProduct())
+	v1.POST("/ship", controllers.CreateDelivery())
+
 }
